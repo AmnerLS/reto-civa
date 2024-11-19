82c36a4ef149db0fed0d2f56ac2ca99d6104abcf
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-11-19 00:08:46 -0500

    Initial commit

---
+++ src/main/java/com/civa/reto/RetoCivaApplication.java
@@ -0,0 +1,13 @@
+package com.civa.reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoCivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/civa/reto/RetoCivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civa.reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
